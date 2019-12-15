9a06b7ac447e22fc4560a6d7c2ae0397173b68b6
cbaoth <dev@cbaoth.de>
2019-12-15 19:47:29 +0100

    Update i3 config

---
+++ .zsh.d/job.zsh
@@ -0,0 +1,16 @@
+# ~/.zsh/functions.zsh: Job functions
+# all functions are written in way that they work on both, zsh and bash
+
+# Author:   cbaoth <dev@cbaoth.de>
+# Keywords: zsh zshrc shell-script functions job
+
+# {{{ = JOB QUEUE ============================================================
+# A simple job queue that should run on all systems without the need for 3rd
+# party software. A simple FIFO solution mainly inteded for 1 prodecer
+# 1 consumer scenarios
+# Source: https://blog.garage-coding.com/2016/02/05/bash-fifo-jobqueue.html
+
+
+# {{{ - COLORS & EFFECTS -----------------------------------------------------
+# }}} - COLORS & EFFECTS -----------------------------------------------------
+# }}} = JOB QUEUE ============================================================
