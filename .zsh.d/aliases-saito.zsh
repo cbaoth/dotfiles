d43617d66835247494c07ed50a40018c0bf150c2
Andreas Weyer <dev@cbaoth.de>
2024-03-14 15:38:50 +0100

    Add zsh saito host aliases

---
+++ .zsh.d/aliases-saito.zsh
@@ -0,0 +1,12 @@
+# ~/.zsh/aliases-saito.zsh: Saito host aliases
+
+# Author:   cbaoth <dev@cbaoth.de>
+# Keywords: zsh zshrc shell-script freebsd
+
+# {{{ - SYSTEM ---------------------------------------------------------------
+alias open-stash="sudo cryptsetup open /dev/md1 stash"
+alias close-stash="sudo cryptsetup close stash"
+alias mount-stash="sudo cryptsetup open /dev/md1 stash && sudo mount /dev/mapper/stash /media/stash"
+alias umount-stash="sudo umount /media/stash; sudo cryptsetup close stash"
+# }}} - SYSTEM ---------------------------------------------------------------
+
