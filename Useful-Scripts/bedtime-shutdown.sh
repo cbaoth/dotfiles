99cb72b3a9735f271f9c4197cca465c22c1f24ee
Andreas Weyer <dev@cbaoth.de>
2025-12-30 18:01:14 +0100

    Update bedtime-shutdown.sh minor improvements and doc

--- Useful-Scripts/bedtime-shutdown.sh
+++ Useful-Scripts/bedtime-shutdown.sh
@@ -3,6 +3,10 @@
 # {{{ = USAGE EXAMPLE ========================================================
 #
 # 1. Put this file into `/opt/bin/bedtime-shutdown.sh`
+#> sudo mkdir -p /opt/bin
+#> sudo cp bedtime-shutdown.sh /opt/bin/bedtime-shutdown.sh
+# Later on, when the file already exists, you can use the following instead (since cp may change file ownership):
+#> sudo tee /opt/bin/bedtime-shutdown.sh <bedtime-shutdown.sh >/dev/null
 #
 # 2. Adjust configuration variables in the "CONFIGURATION" section below.
 #
@@ -43,10 +47,14 @@
 # }}} = USAGE EXAMPLE ========================================================
 
 # {{{ = CONFIGURATION ========================================================
+# {{{ - Mandatory Settings ---------------------------------------------------
+# Conigure the following (mandatory) variables to suit your needs.
+
+# --- User Settings ---
 # User to notify before shutdown
 USER_NAME=cbaoth
-USER_ID=$(id -u $USER_NAME)
 
+# --- Grace Periods ---
 # Grace period (seconds) between user notification and shutdown
 #GRACE_PERIOD_USER=0     # Immediate shutdown without prior warning
 GRACE_PERIOD_USER=180    # 3 minutes
@@ -59,13 +67,15 @@ GRACE_PERIOD_SYSTEM=180  # 3 minutes
 # Format: HHMM (24-hour format), e.g. "2130" = 21:30 (9:30 PM), "0500" = 5:00 (AM)
 # The time the "Bedtime Zone" begins (Shutdown allowed)
 #SHUTDOWN_START=2130
-SHUTDOWN_START=2200
+SHUTDOWN_START=2130
 
 # The time the "Safe Zone" begins (Shutdown skipped)
 #SHUTDOWN_END=0500
 SHUTDOWN_END=0500
+# }}} - Mandatory Settings ---------------------------------------------------
 
-# --- EMERGENCY OVERRIDES (Uncomment to enable) ---
+# {{{ - Optional Emergency Override Settings ---------------------------------
+# Uncomment and adjust any of the following variables to enable emergency shutdown overrides.
 
 # 1. File on USB Stick Override
 # If a specific file is found on any mounted media, shutdown aborts.
@@ -75,20 +85,31 @@ SHUTDOWN_END=0500
 # Adjust filename to look for on the USB stick
 #EMERGENCY_OVERRIDE_FILE_NAME=".bedtime-shutdown.emergency-override.$USER_NAME"
 
-# 2. USB Stick Label Override
-# If a USB stick with this specific LABEL is plugged in, shutdown aborts.
+# 2. USB Flash Drive Label Override
+# If a USB flash drive (or any other block device) with this specific LABEL is plugged in, shutdown aborts.
 # This override is ignored if the variable is not set.
 # Use `lsblk -f` or `lsblk -fno LABEL | grep -v '^$'` to find the labels.
-#EMERGENCY_OVERRIDE_MEDIA_LABEL="NO_SHUTDOWN"
+#EMERGENCY_OVERRIDE_BLOCK_DEVICE_LABEL="NO_SHUTDOWN"
 
 # 3. USB Device ID Override
 # If a USB device with this specific ID is plugged in, shutdown aborts.
 # This override is ignored if the variable is not set.
-# Use `lsusb -t` to find the device ID (format: "1234:abcd").
+# Use `lsusb` (e.g. `lsusb | grep -i realtek`) to find the device ID (format: "1234:abcd").
 #EMERGENCY_OVERRIDE_USB_ID="abcd:1234"
+# }}} - Optional Emergency Override Settings ---------------------------------
+
+# {{{ - Debug Settings -------------------------------------------------------
+# The command used to power off the system
+SYSTEMCTL_POWEROFF_CMD="systemctl poweroff"
+# For testing purposes we can set this to "echo" to prevent actual shutdowns.
+#SYSTEMCTL_POWEROFF_CMD="echo"
+# }}} - Debug Settings -------------------------------------------------------
 # }}} = CONFIGURATION ========================================================
 
-# }}} = SANITY CHECK =========================================================
+# }}} = INITIALIZATION =======================================================
+# Get the user ID of USER_NAME
+USER_ID=$(id -u $USER_NAME)
+
 # Get current time as a number (e.g. 2130 for 21:30 or 9:30 PM)
 CURRENT_TIME=$(date +%H%M)
 
@@ -96,7 +117,7 @@ CURRENT_TIME=$(date +%H%M)
 NOW=$((10#$CURRENT_TIME))
 START=$((10#$SHUTDOWN_START))
 END=$((10#$SHUTDOWN_END))
-# }}} = SANITY CHECK =========================================================
+# }}} = INITIALIZATION =======================================================
 
 # {{{ = FUNCTIONS ============================================================
 # Sends a BEDTIME GUI notification to USER_NAME and broadcasts a message to all terminals.
@@ -150,10 +171,10 @@ _exit_if_safe_zone() {
 _exit_if_emergency_override() {
   # 1. Check for USB Label (Hardware Key with specific LABEL is present)
   # Syntax: [[ -n "$VAR" ]] checks if variable is set and not empty
-  if [[ -n "${EMERGENCY_OVERRIDE_MEDIA_LABEL:-}" ]]; then
+  if [[ -n "${EMERGENCY_OVERRIDE_BLOCK_DEVICE_LABEL:-}" ]]; then
     # We use lsblk to check all block device labels
-    if lsblk -o LABEL -n | grep -Fqx "$EMERGENCY_OVERRIDE_MEDIA_LABEL"; then
-      _notify_user "EMERGENCY OVERRIDE: Hardware key with label '$EMERGENCY_OVERRIDE_MEDIA_LABEL' detected. Skipping shutdown."
+    if lsblk -o LABEL -n | grep -Fqx "$EMERGENCY_OVERRIDE_BLOCK_DEVICE_LABEL"; then
+      _notify_user "EMERGENCY OVERRIDE: Hardware key with label '$EMERGENCY_OVERRIDE_BLOCK_DEVICE_LABEL' detected. Skipping shutdown."
       exit 0
     fi
   fi
@@ -203,7 +224,7 @@ main() {
   # Stage 1: Polite but firm (Ignore Inhibitors, Non-blocking)
   # -i | --ignore-inhibitors: Ignore inhibitors (e.g. "stay awake" apps like Caffeine)
   # --no-block: Send the command and returns IMMEDIATELY (asynchronous), don't wait for response.
-  systemctl poweroff --ignore-inhibitors --no-block
+  $SYSTEMCTL_POWEROFF_CMD --ignore-inhibitors --no-block
 
   # Stage 2: System Grace Period (Wait for graceful shutdown)
   # Skip if GRACE_PERIOD_SYSTEM is set to 0 or less.
@@ -216,7 +237,7 @@ main() {
   # This bypasses all systemd shutdown scripts, unmounting, etc.
   # Skip if GRACE_PERIOD_SYSTEM is set to 0 or less.
   [[ $GRACE_PERIOD_SYSTEM -gt 0 ]] \
-    && systemctl poweroff --force --force
+    && $SYSTEMCTL_POWEROFF_CMD --force --force
 }
 # }}} = FUNCTIONS ============================================================
 
