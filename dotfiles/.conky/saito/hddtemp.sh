2209ee33f900d389ac71af16bfd676718f9f7d78
Andreas Weyer <dev@cbaoth.de>
2026-06-02 18:31:52 +0200

    Enhance conky configs: Improve configs, log and temperature monitoring, add comprehensive helper scripts.

--- dotfiles/.conky/saito/hddtemp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# -*- mode: sh; sh-shell: bash; indent-tabs-mode: nil; tab-width: 2 -*-
-# vim: ft=bash:et:ts=2:sts=2:sw=2
-# code: language=bash insertSpaces=true tabSize=2
-# shellcheck shell=bash
-#
-# .conky/saito/hddtemp.sh: Conky script to get hddtemp output.
-
-sudo hddtemp "$@" 2>/dev/null || echo -1
